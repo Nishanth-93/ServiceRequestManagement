22b2915108b646b7ffb7cceed426baf9825fdced
Aaron Jaeger <jaeger.aaron.m@gmail.com>
2020-07-27 14:13:28 -0500

    Appended exclusions for static classes added to startup and main when cleaning up.

--- scripts/Powershell/Generate-CoverageReport.ps1
+++ scripts/Powershell/Generate-CoverageReport.ps1
@@ -1,5 +1,5 @@
 dotnet restore ./ServiceRequestManagement.sln
 dotnet build ./ServiceRequestManagement.sln --configuration Release --no-restore
-dotnet test ./tests/ServiceRequestManagement.UnitTests/ServiceRequestManagement.UnitTests.csproj --no-restore /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[*]ServiceRequestManagement.Infrastructure.*%2c[*]*Startup%2c[*]*Program%2c[*]ServiceRequestManagement.API.Controllers.*%2c[*]ServiceRequestManagement.API.Application.Behaviors.*"
+dotnet test ./tests/ServiceRequestManagement.UnitTests/ServiceRequestManagement.UnitTests.csproj --no-restore /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[*]ServiceRequestManagement.Infrastructure.*%2c[*]*Startup%2c[*]*Program%2c[*]ServiceRequestManagement.API.Controllers.*%2c[*]ServiceRequestManagement.API.Application.Behaviors.*%2c[*]*ServiceCollectionExtensions%2c[*]*WebHostExtensions"
 reportgenerator "-reports:.\tests\ServiceRequestManagement.UnitTests\coverage.opencover.xml" "-targetdir:.\tests\ServiceRequestManagement.UnitTests\CoverageReports\" "-reporttypes:Html"
 Start-Process .\tests\ServiceRequestManagement.UnitTests\CoverageReports\index.htm
